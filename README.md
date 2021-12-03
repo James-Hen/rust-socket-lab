32cbb9e06f1bbea5f6c3353ab8c27959ffe5845f
yanxwb <yanxwb@qq.com>
2021-12-03 23:28:26 +0800

    Change TCP to UDP and add some intermediate outputs.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rust-socket-lab
 Lab for XJTU computer networks course using rust and extern libc socket
+
+PS: this UDP version may encounter some bugs when the sleeping time of threads aren't in a good condition. 
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ fn main() {
 
         // server
         unsafe {
-            let socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            // let socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            let socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
             if socket < 0 {
                 panic!("last OS error: {:?}", Error::last_os_error());
             }
-
+            println!("Server established");
+            thread::sleep(Duration::from_millis(100));
             let servaddr = sockaddr_in {
                 sin_family: AF_INET as u16,
                 sin_port: 8080u16.to_be(),
@@ -31,55 +33,60 @@ fn main() {
                 println!("last OS error: {:?}", Error::last_os_error());
                 close(socket);
             }
-
-            listen(socket, 128);
-
+            println!("Server binded");
+            println!("Server prepared for receiving");
+            // listen(socket, 128);
             loop {
                 let mut cliaddr: sockaddr_storage = mem::zeroed();
                 let mut len = mem::size_of_val(&cliaddr) as u32;
 
-                let client_socket = accept(socket, &mut cliaddr as *mut sockaddr_storage as *mut sockaddr, &mut len);
-                if client_socket < 0 {
-                    println!("last OS error: {:?}", Error::last_os_error());
-                    break;
-                }
-
+                // let client_socket = accept(socket, &mut cliaddr as *mut sockaddr_storage as *mut sockaddr, &mut len);
+                // if client_socket < 0 {
+                //     println!("last OS error: {:?}", Error::last_os_error());
+                //     break;
+                // }
+                
                 thread::spawn(move || {
                     loop {
                         let mut buf = [0u8; 64];
-                        let n = read(client_socket, &mut buf as *mut _ as *mut c_void, buf.len());
+                        // let n = read(client_socket, &mut buf as *mut _ as *mut c_void, buf.len());
+                        let n = recvfrom(socket, &mut buf as *mut _ as *mut c_void, buf.len(), 0i32, &mut cliaddr as *mut sockaddr_storage as *mut sockaddr, &mut len);
                         if n <= 0 {
                             break;
                         }
-
+                        println!("Server received from client");
                         println!("{:?}", String::from_utf8_lossy(&buf[0..n as usize]));
 
                         let msg = b"Hi, client!";
-                        let n = write(client_socket, msg as *const _ as *const c_void, msg.len());
+                        println!("Server prepared for sending");
+                        // let n = write(client_socket, msg as *const _ as *const c_void, msg.len());
+                        let n = sendto(socket, msg as *const _ as *const c_void, msg.len(), 0i32, &cliaddr as *const sockaddr_storage as *const sockaddr, mem::size_of_val(&cliaddr) as u32);
                         if n <= 0 {
                             break;
                         }
                     }
 
-                    close(client_socket);
+                    // close(client_socket);
                 });
             }
 
-            close(socket);
+            // close(socket);
         }
 
     });
 
-    thread::sleep(Duration::from_secs(1));
+    thread::sleep(Duration::from_millis(100));
 
     // client
     unsafe {
-        let socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        // let socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        let socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if socket < 0 {
             panic!("last OS error: {:?}", Error::last_os_error());
         }
+        println!("Client established");
 
-        let servaddr = sockaddr_in {
+        let mut servaddr = sockaddr_in {
             sin_family: AF_INET as u16,
             sin_port: 8080u16.to_be(),
             sin_addr: in_addr {
@@ -87,26 +94,30 @@ fn main() {
             },
             sin_zero: mem::zeroed()
         };
-
-        let result = connect(socket, &servaddr as *const sockaddr_in as *const sockaddr, mem::size_of_val(&servaddr) as u32);
-        if result < 0 {
-            println!("last OS error: {:?}", Error::last_os_error());
-            close(socket);
-        }
+        let mut len = mem::size_of_val(&servaddr) as u32;
+        // let result = connect(socket, &servaddr as *const sockaddr_in as *const sockaddr, mem::size_of_val(&servaddr) as u32);
+        // if result < 0 {
+        //     println!("last OS error: {:?}", Error::last_os_error());
+        //     close(socket);
+        // }
 
         let msg = b"Hello, server!";
-        let n = write(socket, msg as *const _ as *const c_void, msg.len());
+        println!("Client prepared for sending");
+        // let n = write(socket, msg as *const _ as *const c_void, msg.len());
+        let n = sendto(socket, msg as *const _ as *const c_void, msg.len(), 0i32, &servaddr as *const sockaddr_in as *const sockaddr, mem::size_of_val(&servaddr) as u32);
         if n <= 0 {
             println!("last OS error: {:?}", Error::last_os_error());
             close(socket);
         }
 
         let mut buf = [0u8; 64];
-        let n = read(socket, &mut buf as *mut _ as *mut c_void, buf.len());
+        println!("Client prepared for receiving");
+        // let n = read(socket, &mut buf as *mut _ as *mut c_void, buf.len());
+        let n = recvfrom(socket, &mut buf as *mut _ as *mut c_void, buf.len(), 0i32, &mut servaddr as *mut sockaddr_in as *mut sockaddr, &mut len);
         if n <= 0 {
             println!("last OS error: {:?}", Error::last_os_error());
         }
-
+        println!("Client received from server");
         println!("{:?}", String::from_utf8_lossy(&buf[0..n as usize]));
 
         close(socket);
