c74f70cfc1cbbcfabf010615513435a070a661d5
yanxwb <yanxwb@qq.com>
2021-12-04 23:37:00 +0800

    Add 3 files to separate server and client from main.rs, using TCP.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rust-socket-lab
 Lab for XJTU computer networks course using rust and extern libc socket
+
+Using TCP.
+
+Currently, by using cargo run, we go to the menu. By printing different numbers, each terminal could be server or client to communicate.
+
+PS: 
+
+1. UI is ugly now.
+
+2. Client don't need ip and port. And we should add this feature later.
+3. We need log in feature.
---
+++ src/client.rs
@@ -0,0 +1,50 @@
+use crate::unix_socket::*;
+use std::io::Error;
+use std::mem;
+use std::thread;
+use std::time::Duration;
+
+pub fn start(){
+    unsafe {
+        let socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if socket < 0 {
+            panic!("last OS error: {:?}", Error::last_os_error());
+        }
+        println!("Client established");
+
+        let servaddr = sockaddr_in {
+            sin_family: AF_INET as u16,
+            sin_port: 8080u16.to_be(),
+            sin_addr: in_addr {
+                s_addr: u32::from_be_bytes([127, 0, 0, 1]).to_be()
+            },
+            sin_zero: mem::zeroed()
+        };
+
+        let result = connect(socket, &servaddr as *const sockaddr_in as *const sockaddr, mem::size_of_val(&servaddr) as u32);
+        if result < 0 {
+            println!("last OS error: {:?}", Error::last_os_error());
+            close(socket);
+        }
+        println!("Client connected to server");
+
+        let msg = b"Hello, server!";
+        println!("Client prepared for sending");
+        let n = write(socket, msg as *const _ as *const c_void, msg.len());
+        if n <= 0 {
+            println!("last OS error: {:?}", Error::last_os_error());
+            close(socket);
+        }
+        println!("Client sended 'Hello, server!' successfully");
+        let mut buf = [0u8; 64];
+        println!("Client prepared for receiving");
+        let n = read(socket, &mut buf as *mut _ as *mut c_void, buf.len());
+        if n <= 0 {
+            println!("last OS error: {:?}", Error::last_os_error());
+        }
+        println!("Client received from server");
+        println!("{:?}", String::from_utf8_lossy(&buf[0..n as usize]));
+        println!("");
+        close(socket);
+    }
+}
